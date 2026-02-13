d8437e3b9631be4a788f63b212fbe509ae462706
Sweden <sweden.agunenye1@gmail.com>
2026-02-12 21:05:53 -0800

    formatting + check where blank is

--- puzzle.py
+++ puzzle.py
@@ -3,15 +3,21 @@ from treelib import Tree
 from array import *
 
 problem = []
-goalState = [[1, 2, 3], [4, 5, 6], [7, 8, 0]] # Hardcoded goal state of eight puzzle, used to test if we found solution
+goalState = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 0],
+]  # Hardcoded goal state of eight puzzle, used to test if we found solution
 puzzles = Tree()
 
-class Puzzle: # Generic puzzle of size n^2 - 1
+
+class Puzzle:  # Generic puzzle of size n^2 - 1
     def __init__(self, state: list[list[int]]):
         self.state = state
 
     def __str__(self):
         return self.join(map(str, self))
+
 
 # pseudocode
 # function general-search(problem, QUEUEING-FUNCTION)
@@ -23,21 +29,42 @@ class Puzzle: # Generic puzzle of size n^2 - 1
 # 	nodes = QUEUEING-FUNCTION(nodes, EXPAND(node, problem.OPERATORS))
 # end
 
-def general_search(problem: list[list[int]], queuing_function): # queuing function implementations are uniform cost search, A* with the
-                                               # Misplaced Tile heuristic, and A* with the Manhattan distance heuristic
-    initialState = Puzzle(problem)
-    nodes = pq(puzzles.create_node("root", data={"state": initialState.state})) # I used a tree implementation to initialize the
-                                             #  first game state of the puzzle, which can be any test case
 
-    while (True):
-        if not nodes: # Search function should fail if there are no more game states to explore (no solution!)
-            return False # might need a better return value
+def general_search(
+    problem: list[list[int]], queuing_function
+):  # queuing function implementations are uniform cost search, A* with the
+    # Misplaced Tile heuristic, and A* with the Manhattan distance heuristic
+    initialState = Puzzle(problem)
+    nodes = pq(
+        puzzles.create_node("root", data={"state": initialState.state})
+    )  # I used a tree implementation to initialize the
+    #  first game state of the puzzle, which can be any test case
+
+    while True:
+        if (
+            not nodes
+        ):  # Search function should fail if there are no more game states to explore (no solution!)
+            return False  # might need a better return value
         node = nodes.pop()
 
-        if (goalState == node.state): # Are we at the solution yet? Compares goal/solution state to current state of puzzle
+        if (
+            goalState == node.state
+        ):  # Are we at the solution yet? Compares goal/solution state to current state of puzzle
             return node.state
-        
-        nodes = uniform_cost(nodes, )
+
+        nodes = uniform_cost(
+            nodes,
+        )
+
+
+# We need to know the position of the "blank" tile in order to perform an operation (move it in one of four directions)
+def check_blank(problem: list[list[int]]):
+    for row in problem:
+        for col in row:
+            if problem[row][col] == 0:
+                return row, col
+
+    return None
 
 
 # def move_up(problem: list[list[int]]) -> list[list[int]]:
@@ -76,8 +103,11 @@ def general_search(problem: list[list[int]], queuing_function): # queuing functi
 
 #     return problem
 
-def expand(head: Puzzle): # Creates all children of current node "head" where each child is a different single operator applied
-                    # to input state
+
+def expand(
+    head: Puzzle,
+):  # Creates all children of current node "head" where each child is a different single operator applied
+    # to input state
     puzzles.create_node(data={"state": move_left(head.state)})
     puzzles.create_node(data={"state": move_right(head.state)})
     puzzles.create_node(data={"state": move_up(head.state)})
--- searches.py
+++ searches.py
@@ -1,8 +1,16 @@
-def uniform_cost(search_space, ): # Uniform cost search
+def uniform_cost(
+    search_space,
+):  # Uniform cost search
     pass
 
-def misplaced_tile(search_space, ): # A* search with Misplaced Tile heuristic
+
+def misplaced_tile(
+    search_space,
+):  # A* search with Misplaced Tile heuristic
     pass
 
-def manhattan_dist(search_space, ): # A* search with Manhattan distance heuristic
+
+def manhattan_dist(
+    search_space,
+):  # A* search with Manhattan distance heuristic
     pass
